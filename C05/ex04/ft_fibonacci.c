38857f1fb334780dcb59162a45eea153b11286da
aminfdy0 <aminefdy@gmail.com>
2026-03-14 16:32:04 +0000

    ex04 - Fibonacci By Using Recursion

---
+++ C05/ex04/ft_fibonacci.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fdycoding <aminefdy@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/14 16:17:29 by fdycoding         #+#    #+#             */
+/*   Updated: 2026/03/14 16:17:32 by fdycoding        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	if (index <= 1)
+		return (index);
+	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
