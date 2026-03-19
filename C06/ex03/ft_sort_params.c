1334f7e551da12acb762e3472a051ed13c2545e3
aminfdy0 <aminefdy@gmail.com>
2026-03-19 16:41:14 +0000

    ex03 - ft_sort_params

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fdycoding <aminefdy@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/19 16:38:53 by fdycoding         #+#    #+#             */
+/*   Updated: 2026/03/19 16:40:10 by fdycoding        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+int main(int ac, char **av)
+{
+	int	i;
+	int	j;
+	char	*temp;
+
+	i = 1;
+	while (i < ac - 1)
+	{
+		j = i + 1;
+		while (j < ac)
+		{
+			if (ft_strcmp(av[i], av[j]) > 0)
+			{
+				temp = av[i];
+				av[i] = av[j];
+				av[j] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+	i = 1;
+	while (i <= ac - 1)
+		ft_putstr(av[i++]);
+}
